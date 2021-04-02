8969f75d7ad4ff7495eec7444513c670bd0edece
avinash_juspay <avinash.verma@juspay.in>
2021-04-02 10:39:36 +0530

    auth API structure is defined

--- controller/V1/authAPI.js
+++ controller/V1/authAPI.js
@@ -1,6 +1,29 @@
+import _ from 'lodash'
 
 
+export function signUp(req, res, next) {
+    var name = _.trim(req.body.name);
+    var email = _.trim(req.body.email);
+    var passsword = _.trim(req.body.passsword);
 
-function signUp(req, res, next) {
+}
+
+
+export function login(req, res, next){
+    var email = _.trim(req.body.email);
+    var password = _.trim(req.body.passsword);
+
+}
+
+export function logout(req, res, next){
     
 }
+
+export function resetPassword(req, res, next){
+    var email = _.trim(req.body.email);
+}
+
+export function updatePassword(req, res, next){
+    
+}
+
--- controller/V1/mainCont.js
+++ controller/V1/mainCont.js
@@ -1,5 +1,14 @@
+import { login, signUp } from "./authAPI";
 
 
-function authControllers(router){
-    router.post('/register', ()=>{})
+function authControllers(router) {
+    router.post( '/register'   , signUp);
+    router.patch( '/login'     , login);
+    router.delete( '/logout'   , logout);
+    router.patch( '/resetpass' , resetPassword);
+    router.patch( '/updatePass', updatePassword);
 }
+
+
+
+
